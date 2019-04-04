879bbca3e6a1292c9ddf26440a41f5c12286e6c7
KagaK2 <KhanhPhan2512@gmail.com>
2019-04-04 13:25:16 +0300

    Add a button to remove each to do item

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ console.log("KENDRICK LAMAR");
 formSubmit = event => {
   event.preventDefault();
   console.log(event.target[0].value);
-  let toDoItem = document.createElement("p");
+  let toDoItem = document.createElement("div");
+  let removeButton = document.createElement("button");
+  removeButton.innerHTML = "X";
+  removeButton.onclick = event => removeParent(event);
   toDoItem.innerHTML = event.target[0].value;
+  toDoItem.appendChild(removeButton);
   document.getElementById("todo-list").appendChild(toDoItem);
+};
+
+removeParent = event => {
+  console.log(
+    event.target.parentNode.parentNode.removeChild(event.target.parentNode)
+  );
 };
